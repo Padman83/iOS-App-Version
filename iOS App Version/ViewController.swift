263a0658e25fe21396ffaf86be4d7a5773fe6136
Padman Selvamanickam <endlessgalaxy15@gmail.com>
2020-02-02 23:20:10 +0800

    Commit 4 Files.

--- iOS App Version/ViewController.swift
+++ iOS App Version/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let kVersion = "CFBundleShortVersionString"
+    let kBuildNumber = "CFBundleVersion"
+    
+    @IBOutlet weak var versionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        versionLabel.text = getVersion()
     }
-
+    
+    func getVersion() -> String {
+        let dictionary = Bundle.main.infoDictionary!
+        let version = dictionary[kVersion] as! String
+        let build = dictionary[kBuildNumber] as! String
+        
+        return "\(version)(\(build))"
+    }
 
 }
 
